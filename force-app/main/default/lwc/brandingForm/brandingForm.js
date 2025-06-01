d849a7f0aa6ac37c573e607beb708235208965b0
Vadym Prylozhenko <vadymprylozhenko@Vadyms-MacBook-Pro.local>
2025-06-02 01:09:05 +0200

    first commit

---
+++ force-app/main/default/lwc/brandingForm/brandingForm.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+import sendToExternalApi from '@salesforce/apex/BrandingFormController.sendToExternalApi';
+
+export default class BrandingForm extends LightningElement {
+    uploadedFiles = [];
+    selectedColor;
+
+    handleUploadFinished(event) {
+        const files = event.detail.files;
+        this.uploadedFiles.push(...files.map(f => f.documentId));
+    }
+
+    handleColorChange(event) {
+        this.selectedColor = event.target.value;
+    }
+
+    handleSubmit() {
+        sendToExternalApi({ 
+            fileIds: this.uploadedFiles, 
+            color: this.selectedColor 
+        }).then(() => {
+            console.log('Data sent to receiver org');
+        }).catch(err => {
+            console.error('Error sending data', err);
+        });
+    }
+}
